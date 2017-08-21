a2139d33a9246da88426744ae55a5ddb8e9cf378
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2017-08-21 13:33:23 +0300

    added indent rule; updated package version in preparation to publish;

--- tslint.js
+++ tslint.js
@@ -11,6 +11,7 @@ module.exports = {
             {"multiline": "never"},
             {"singleline": "never"}
         ],
+        "indent": [true, "spaces", 4],
         "interface-name": [false],
         "jsx-no-multiline-js": false,
         "no-empty": false,
